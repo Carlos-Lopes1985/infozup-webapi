9b1aae9b0a195ee53a4fb33d159547b58230b70c
Carlos-Lopes1985 <carlosslopes1985@hotmail.com>
2022-09-22 01:29:17 -0300

    primeiro commit

---
+++ src/main/java/com/infozup/webapi/PocWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.infozup.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocWebApiApplication.class, args);
+	}
+
+}
